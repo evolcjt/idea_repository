bf328e8db5ae0c8b6c6b797638494c503d1e9c82
cjt <936251195@qq.com>
2019-07-12 20:38:45 +0800

    在idea中第三次修改

--- git_test/src/com/itheima/test/Test.java
+++ git_test/src/com/itheima/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("test...");
         System.out.println("test1...");
         System.out.println("test2...");
+        System.out.println("test3...");
     }
 }
